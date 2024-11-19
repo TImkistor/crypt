36a78f1391b475ae55178257ce94018b52025746
Timkistor <tim.kisslove@gmail.com>
2024-11-20 01:37:45 +0300

    fix front 0.2

--- back.js
+++ back.js
@@ -30,14 +30,14 @@ function selectTextFunction(selectText) {
   switch (selectText) {
     case "aes":
       return {
-        hTwo_display: "block",
+        hTwo_display: "flex",
         textAreaKey_display: "block",
         buttonDecrypt_display: "block",
         hEncrypt_innerText: "Защифровать",
       };
     case "rs4":
       return {
-        hTwo_display: "block",
+        hTwo_display: "flex",
         textAreaKey_display: "block",
         buttonDecrypt_display: "flex",
         hEncrypt_innerText: "Защифровать",
