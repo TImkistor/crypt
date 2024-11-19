25a0f767943ee86f3d5708e505a0aa4a950362b3
rus_anonym <rus_cybersec@vk.com>
2024-11-20 00:37:20 +0300

    fixes cheel troubles

--- back.js
+++ back.js
@@ -1,56 +1,49 @@
-
-function encrypt(selectText,startText,keyText) {
-    let overText = "Прив"
-    switch(selectText){
-        case "aes":
-            overText = "Прив"
-            return overText
-        case "rs4":
-            overText = "Прив"
-            return overText
-        case "md5":
-            overText = "ghbd"
-            return overText
-        case "sha256":
-            overText = "Прив"
-            return overText
-        default:
-            overText = "Что то пошло не так попробуйте снова:("
-            return overText  
-    }
+function encrypt(selectText, startText, keyText) {
+  let overText = "Прив";
+  switch (selectText) {
+    case "aes":
+      return CryptoJS.AES.encrypt(selectText, keyText).toString();
+    case "rs4":
+      return CryptoJS.RC4.encrypt(selectText, keyText).toString();
+    case "md5":
+      return CryptoJS.SHA256(selectText).toString();
+    case "sha256":
+      return CryptoJS.SHA256(selectText).toString();
+    default:
+      overText = "Что то пошло не так попробуйте снова:(";
+      return overText;
+  }
 }
-function selectTextFunction(selectText){
-    switch(selectText){
-        case "aes":
-            return{
-             hTwo_display: "block",
-             textAreaKey_display: "block",
-             buttonDecrypt_display: "block",
-             hEncrypt_innerText: "Защифровать",
-            }
-        case "rs4":
-            return{
-             hTwo_display: "block",
-             textAreaKey_display: "block",
-             buttonDecrypt_display: "block",
-             hEncrypt_innerText: "Защифровать",
-            }
-        case "sha256":
-            return{
-             hTwo_display: "none",
-             textAreaKey_display: "none",
-             buttonDecrypt_display: "none",
-             hEncrypt_innerText: "Получить ХЕШ",
-            }
-        case "md5":
-            return{
-             hTwo_display: "none",
-             textAreaKey_display: "none",
-             buttonDecrypt_display: "none",
-             hEncrypt_innerText: "Получить ХЕШ",
-            }
-    }
+function selectTextFunction(selectText) {
+  switch (selectText) {
+    case "aes":
+      return {
+        hTwo_display: "block",
+        textAreaKey_display: "block",
+        buttonDecrypt_display: "block",
+        hEncrypt_innerText: "Защифровать",
+      };
+    case "rs4":
+      return {
+        hTwo_display: "block",
+        textAreaKey_display: "block",
+        buttonDecrypt_display: "block",
+        hEncrypt_innerText: "Защифровать",
+      };
+    case "sha256":
+      return {
+        hTwo_display: "none",
+        textAreaKey_display: "none",
+        buttonDecrypt_display: "none",
+        hEncrypt_innerText: "Получить ХЕШ",
+      };
+    case "md5":
+      return {
+        hTwo_display: "none",
+        textAreaKey_display: "none",
+        buttonDecrypt_display: "none",
+        hEncrypt_innerText: "Получить ХЕШ",
+      };
+  }
 }
-export {encrypt}
-export {selectTextFunction}
-
+export { encrypt, selectTextFunction };
--- front.js
+++ front.js
@@ -1,5 +1,5 @@
-import { encrypt,selectTextFunction } from "./back.js";
-const optionStorage = ["aes","rs4",'sha256','md5']
+import { encrypt, selectTextFunction } from "./back.js";
+const optionStorage = ["aes", "rs4", "sha256", "md5"];
 
 // Элементы страницы
 const divMain = document.createElement("div");
@@ -18,134 +18,158 @@ const buttonEncrypt = document.createElement("div");
 
 // Стили div
 divMain.id = "myDiv";
-divMain.style.backgroundColor = "rgb(20, 16, 39)"
-divMain.style.width = "90%"
-divMain.style.height = "100%"
-divMain.style.borderRadius = "5%"
-divMain.style.marginLeft = "5%"
+divMain.style.backgroundColor = "rgb(20, 16, 39)";
+divMain.style.width = "90%";
+divMain.style.height = "100%";
+divMain.style.borderRadius = "5%";
+divMain.style.marginLeft = "5%";
 
 // Стили h1
-hOne.innerText = "Алгоритм"
-hTwo.innerText = "Ключ"
-hThree.innerText = "Ввод"
-hFour.innerText = "Ввывод"
-hEncrypt.innerText = "Защифровать" 
-hDecrypt.innerText = "Расщифровать"
-hTwo.id = "hTwo"
-hOne.style.textAlign=hTwo.style.textAlign=hThree.style.textAlign=hFour.style.textAlign = hEncrypt.style.textAlign = hDecrypt.style.textAlign = "center"
-hOne.style.color=hTwo.style.color=hThree.style.color=hFour.style.color = "white"
+hOne.innerText = "Алгоритм";
+hTwo.innerText = "Ключ";
+hThree.innerText = "Ввод";
+hFour.innerText = "Ввывод";
+hEncrypt.innerText = "Защифровать";
+hDecrypt.innerText = "Расщифровать";
+hTwo.id = "hTwo";
+hOne.style.textAlign =
+  hTwo.style.textAlign =
+  hThree.style.textAlign =
+  hFour.style.textAlign =
+  hEncrypt.style.textAlign =
+  hDecrypt.style.textAlign =
+    "center";
+hOne.style.color =
+  hTwo.style.color =
+  hThree.style.color =
+  hFour.style.color =
+    "white";
 
 // Стили select
 selectMain.style.width = "80%";
 selectMain.style.height = "3%";
-selectMain.style.marginLeft = "10%"
-selectMain.style.backgroundColor = "black"
-selectMain.style.color = "white"
-selectMain.style.fontSize = "150%"
-selectMain.id = "selectMain"
+selectMain.style.marginLeft = "10%";
+selectMain.style.backgroundColor = "black";
+selectMain.style.color = "white";
+selectMain.style.fontSize = "150%";
+selectMain.id = "selectMain";
 
 //Стили TextArea
-textAreaKey.style.width = textAreaStart.style.width = textAreaOver.style.width = "80%"
-textAreaKey.style.marginLeft = textAreaStart.style.marginLeft = textAreaOver.style.marginLeft = "10%"
-textAreaKey.style.backgroundColor = textAreaStart.style.backgroundColor = textAreaOver.style.backgroundColor= "black"
-textAreaKey.style.color =  textAreaStart.style.color = textAreaOver.style.color = "white"
-textAreaStart.style.height = textAreaOver.style.height = "20%"
-textAreaKey.style.height = "5%"
-textAreaKey.id = "inputKey"
-textAreaStart.id = "inputStart"
-textAreaOver.id = "inputOver"
+textAreaKey.style.width =
+  textAreaStart.style.width =
+  textAreaOver.style.width =
+    "80%";
+textAreaKey.style.marginLeft =
+  textAreaStart.style.marginLeft =
+  textAreaOver.style.marginLeft =
+    "10%";
+textAreaKey.style.backgroundColor =
+  textAreaStart.style.backgroundColor =
+  textAreaOver.style.backgroundColor =
+    "black";
+textAreaKey.style.color =
+  textAreaStart.style.color =
+  textAreaOver.style.color =
+    "white";
+textAreaStart.style.height = textAreaOver.style.height = "20%";
+textAreaKey.style.height = "5%";
+textAreaKey.id = "inputKey";
+textAreaStart.id = "inputStart";
+textAreaOver.id = "inputOver";
 
 //Стили кнопок
-buttonEncrypt.style.display = buttonDecrypt.style.display = "flex"
-buttonEncrypt.style.justifyContent = buttonDecrypt.style.justifyContent = "center"
-buttonEncrypt.style.alignItems = buttonDecrypt.style.alignItems = "center"
-buttonEncrypt.style.width = buttonDecrypt.style.width = "30%" 
-buttonEncrypt.style.height = buttonDecrypt.style.height = "5%"
-buttonEncrypt.style.marginLeft = buttonDecrypt.style.marginLeft = "35%"
-buttonEncrypt.style.marginTop = buttonDecrypt.style.marginTop = "2%"
-buttonEncrypt.style.borderRadius = buttonDecrypt.style.borderRadius = "10%"
-buttonEncrypt.style.fontSize = buttonDecrypt.style.fontSize = "130%"
-buttonEncrypt.style.backgroundColor = buttonDecrypt.style.backgroundColor = "green"
-buttonEncrypt.style.color = buttonDecrypt.style.color = "white"
-buttonEncrypt.id = "buttonEncrypt"
-buttonDecrypt.id = "buttonDecrypt"
+buttonEncrypt.style.display = buttonDecrypt.style.display = "flex";
+buttonEncrypt.style.justifyContent = buttonDecrypt.style.justifyContent =
+  "center";
+buttonEncrypt.style.alignItems = buttonDecrypt.style.alignItems = "center";
+buttonEncrypt.style.width = buttonDecrypt.style.width = "30%";
+buttonEncrypt.style.height = buttonDecrypt.style.height = "5%";
+buttonEncrypt.style.marginLeft = buttonDecrypt.style.marginLeft = "35%";
+buttonEncrypt.style.marginTop = buttonDecrypt.style.marginTop = "2%";
+buttonEncrypt.style.borderRadius = buttonDecrypt.style.borderRadius = "10%";
+buttonEncrypt.style.fontSize = buttonDecrypt.style.fontSize = "130%";
+buttonEncrypt.style.backgroundColor = buttonDecrypt.style.backgroundColor =
+  "green";
+buttonEncrypt.style.color = buttonDecrypt.style.color = "white";
+buttonEncrypt.id = "buttonEncrypt";
+buttonDecrypt.id = "buttonDecrypt";
 
 //Анимация для кнопки Encrypt
-buttonEncrypt.addEventListener("mouseover", function() {
-    buttonEncrypt.style.backgroundColor = "rgb(9, 30, 16)";
-    });
-    
-buttonEncrypt.addEventListener("mouseleave", function() {
-    buttonEncrypt.style.backgroundColor = "green";
-    });
-    
-buttonEncrypt.addEventListener("click", function() {
-    buttonEncrypt.style.backgroundColor = "black";
-    encryptFunction()          
-    });
+buttonEncrypt.addEventListener("mouseover", function () {
+  buttonEncrypt.style.backgroundColor = "rgb(9, 30, 16)";
+});
+
+buttonEncrypt.addEventListener("mouseleave", function () {
+  buttonEncrypt.style.backgroundColor = "green";
+});
+
+buttonEncrypt.addEventListener("click", function () {
+  buttonEncrypt.style.backgroundColor = "black";
+  encryptFunction();
+});
 
 //Анимация для кнопки Decrypt
-buttonDecrypt.addEventListener("mouseover", function() {
-        buttonDecrypt.style.backgroundColor = "rgb(9, 30, 16)";
-        });
-        
-buttonDecrypt.addEventListener("mouseleave", function() {
-        buttonDecrypt.style.backgroundColor = "green";
-        });
-        
-buttonDecrypt.addEventListener("click", function() {
-        buttonDecrypt.style.backgroundColor = "black";
-        });
+buttonDecrypt.addEventListener("mouseover", function () {
+  buttonDecrypt.style.backgroundColor = "rgb(9, 30, 16)";
+});
+
+buttonDecrypt.addEventListener("mouseleave", function () {
+  buttonDecrypt.style.backgroundColor = "green";
+});
+
+buttonDecrypt.addEventListener("click", function () {
+  buttonDecrypt.style.backgroundColor = "black";
+});
 
 // Аниимация для selectMain
-selectMain.addEventListener("click", function(){
-let selectText = document.getElementById("selectMain").value
-let param = selectTextFunction(selectText)
-hTwo.style.display = param.hTwo_display
-textAreaKey.style.display = param.textAreaKey_display
-buttonDecrypt.style.display = param.buttonDecrypt_display
-hEncrypt.innerText = param.hEncrypt_innerText
-})
+selectMain.addEventListener("click", function () {
+  let selectText = document.getElementById("selectMain").value;
+  let param = selectTextFunction(selectText);
+  hTwo.style.display = param.hTwo_display;
+  textAreaKey.style.display = param.textAreaKey_display;
+  buttonDecrypt.style.display = param.buttonDecrypt_display;
+  hEncrypt.innerText = param.hEncrypt_innerText;
+});
 
 // Функция ввывода всех элемнтов при загрузке страницы
-function main(){
-    document.body.appendChild(divMain);
+function main() {
+  document.body.appendChild(divMain);
 
-    divMain.appendChild(hOne);
+  divMain.appendChild(hOne);
 
-    document.body.appendChild(selectMain);
-    divMain.appendChild(selectMain)
-    optionStorage.forEach(option => {
-        const newOption = document.createElement("option");
-        newOption.innerText = option;
-        newOption.style.textAlignLast = "center"
-        selectMain.appendChild(newOption);
-        });
-    divMain.appendChild(hTwo)
+  document.body.appendChild(selectMain);
+  divMain.appendChild(selectMain);
+  optionStorage.forEach((option) => {
+    const newOption = document.createElement("option");
+    newOption.innerText = option;
+    newOption.style.textAlignLast = "center";
+    selectMain.appendChild(newOption);
+  });
+  divMain.appendChild(hTwo);
 
-    divMain.appendChild(textAreaKey)
+  divMain.appendChild(textAreaKey);
 
-    divMain.appendChild(hThree)
+  divMain.appendChild(hThree);
 
-    divMain.appendChild(textAreaStart)
+  divMain.appendChild(textAreaStart);
 
-    divMain.appendChild(hFour)
+  divMain.appendChild(hFour);
 
-    divMain.appendChild(textAreaOver)
+  divMain.appendChild(textAreaOver);
 
-    divMain.appendChild(buttonEncrypt)
-    buttonEncrypt.appendChild(hEncrypt)
+  divMain.appendChild(buttonEncrypt);
+  buttonEncrypt.appendChild(hEncrypt);
 
-    divMain.appendChild(buttonDecrypt)
-    buttonDecrypt.appendChild(hDecrypt)     
+  divMain.appendChild(buttonDecrypt);
+  buttonDecrypt.appendChild(hDecrypt);
 }
 window.onload = main;
 
-function encryptFunction(){
- let selectText = document.getElementById("selectMain").value
- let startText = document.getElementById("inputStart").value
- let keyText = document.getElementById("inputKey").value
+function encryptFunction() {
+  let selectText = document.getElementById("selectMain").value;
+  let startText = document.getElementById("inputStart").value;
+  let keyText = document.getElementById("inputKey").value;
 
- let overText = encrypt(selectText,startText,keyText);
- console.log(overText)
+  let overText = encrypt(selectText, startText, keyText);
+  console.log(overText);
 }
